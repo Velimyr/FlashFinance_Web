8ae95aad7e637f30ca94919ca14740da1bc18edc
v.tymoshchuk <v.tymoshchuk@softlist.com.ua>
2016-02-25 17:42:57 +0200

    localization classes add some css add

--- FlashFinance_Web/App_Start/RouteConfig.cs
+++ FlashFinance_Web/App_Start/RouteConfig.cs
@@ -14,9 +14,16 @@ namespace FlashFinance_Web
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "lang",
+                url: "{lang}/{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                constraints: new { lang = @"ua|en|ru" }
+                );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional , lang="ua"}
             );
         }
     }
--- FlashFinance_Web/Controllers/HomeController.cs
+++ FlashFinance_Web/Controllers/HomeController.cs
@@ -1,19 +1,51 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 //using FlashFinance_Web.Models;
 
 namespace FlashFinance_Web.Controllers
 {
     public class HomeController : Controller
     {
-        FlashFinanceEntities db = new FlashFinanceEntities();
+        public string CurrentLangCode { get; protected set; }
+        
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+           if (requestContext.RouteData.Values["lang"] != null && requestContext.RouteData.Values["lang"] as string != "null")
+            {
+                CurrentLangCode = requestContext.RouteData.Values["lang"] as string;
+                switch (CurrentLangCode)
+                {
+                    case "ua":
+                        CurrentLangCode = "uk-UA";
+                        break;
+                    case "en":
+                        CurrentLangCode = "en-GB";
+                        break;
+                    case "ru":
+                        CurrentLangCode = "ru-RU";
+                        break;
+                    default:
+                        CurrentLangCode = "uk-UA";
+                        break;
+                }
+
+                var ci = new CultureInfo(CurrentLangCode);
+                Thread.CurrentThread.CurrentUICulture = ci;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
+            }
+            base.Initialize(requestContext);
+        }
+      FlashFinanceEntities db = new FlashFinanceEntities();
         public ActionResult Index()
         {
-           // IEnumerable<Bills> bills = db.Bills;
             ViewBag.Bills = db.Bills;
             ViewBag.Registers = db.Registers;
             return View();
@@ -22,14 +54,12 @@ namespace FlashFinance_Web.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
@@ -41,16 +71,14 @@ namespace FlashFinance_Web.Controllers
 
         [HttpPost]
         public string AddRegister (Registers RegisterItem)
-        {            
-            //if (RegisterItem.Id == 0)
-            //{
-            //    int MaxID = db.Registers.Max(r => r.Id);
-            //    RegisterItem.Id = MaxID+1;
-            //}
-                       
+        {                       
             db.Registers.Add(RegisterItem);
             db.SaveChanges();
             return "Ok";
         }
+
+
+ 
+      
     }
 }
