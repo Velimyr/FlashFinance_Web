8cc6d007e2b32e978f80503fcdbe407e67925cdc
v.tymoshchuk <v.tymoshchuk@softlist.com.ua>
2016-03-09 11:53:16 +0200

    fixed ajax request for filter register list

--- FlashFinance_Web/Controllers/RegisterController.cs
+++ FlashFinance_Web/Controllers/RegisterController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,7 +10,34 @@ namespace FlashFinance_Web.Controllers
 {
     public class RegisterController : Controller
     {
+        public string CurrentLangCode { get; protected set; }
+        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        {
+            if (requestContext.RouteData.Values["lang"] != null && requestContext.RouteData.Values["lang"] as string != "null")
+            {
+                CurrentLangCode = requestContext.RouteData.Values["lang"] as string;
+                switch (CurrentLangCode)
+                {
+                    case "ua":
+                        CurrentLangCode = "uk-UA";
+                        break;
+                    case "en":
+                        CurrentLangCode = "en-GB";
+                        break;
+                    case "ru":
+                        CurrentLangCode = "ru-RU";
+                        break;
+                    default:
+                        CurrentLangCode = "uk-UA";
+                        break;
+                }
 
+                var ci = new CultureInfo(CurrentLangCode);
+                Thread.CurrentThread.CurrentUICulture = ci;
+                Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
+            }
+            base.Initialize(requestContext);
+        }
         FlashFinanceEntities db = new FlashFinanceEntities();
 
         // GET: Register
@@ -17,7 +46,7 @@ namespace FlashFinance_Web.Controllers
             return View();
         }
 
-
+        [HttpPost]
         public ActionResult FilterRegister(DateTime begin , DateTime end)
         {
             if (begin==null)
@@ -28,9 +57,33 @@ namespace FlashFinance_Web.Controllers
             {
                 end = DateTime.Parse(DateTime.Now.Year + "-12-31");
             }
-            ViewBag.Bills = db.Bills;
+            //ViewBag.Bills = db.Bills;
             ViewBag.Registers = db.P_FlashFinance_Registers_Get(begin, end);
-            return View("~/Views/Home/Index.cshtml");
+
+           // return View("~/Views/Home/Index.cshtml");
+            var json_data = Json(ViewBag.Registers);
+            return json_data;
+        }
+
+        [HttpPost]
+        public ActionResult FilterRegister_NoJquery()
+        {
+            string begin = Request["begin"].ToString();
+            string end = Request["end"].ToString();
+            if ((begin == null) || (begin==""))
+            {
+                begin = DateTime.Parse(DateTime.Now.Year + "-01-01").ToString();
+            }
+            if ((end == null)|| (end == ""))
+            {
+                end = DateTime.Parse(DateTime.Now.Year + "-03-31").ToString();
+            }
+            ViewBag.Bills = db.Bills;
+            ViewBag.Registers = db.P_FlashFinance_Registers_Get(DateTime.Parse(begin), DateTime.Parse(end));
+
+            return PartialView("~/Views/Home/Index.cshtml");
+            
         }
     }
 }
+
--- FlashFinance_Web/Scripts/User/Register.js
+++ FlashFinance_Web/Scripts/User/Register.js
@@ -21,12 +21,61 @@ function FilterValidate() {
 
 
 $(document).ready(function () {
-    $('#submit_filter').click(function (e) {
-        e.preventDefault();
-        var begin = $('#datepicker').val();
-        var begin = $('#datepicker_end').val();
+
+    //$('#submit_filter').click(function () {
+    //    //e.preventDefault();
+    //    var begin = $('#datepicker').val();
+    //    var end = $('#datepicker_end').val();
       
-        $('#row_registers').load('@Url.Action("FilterRegister", "Register")?begin=' + begin + '&end=' + end);
-    });
+    //    $('#myPartialViewContainer').load('@Url.Action("FilterRegister", "Register")?begin=' + begin + '&end=' + end);
+    //});
+
+   // $('#refresh').bind('click', RefreshRegisters);
 });
+
+
+function RefreshRegisters() {
+            $.ajax({
+                url: '/Register/FilterRegister',
+                type: 'POST',
+                data: '{ '
+                + '"begin" : "' + $('#datepicker').val() + '", '
+                + '"end" : "' + $('#datepicker_end').val() + '" '
+                + '} ',
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                error: function (xhr) {
+                    error = xhr;
+                    alert('Error: ' + xhr.text);                   
+                },
+                success: function (result) {                                     
+                       // alert('Status Ok: ' + result);                       
+                    //===============================================================================
+                    //Оновлення таблиці видатків записами отриманими через Ajax+jQuery
+                    //===============================================================================
+                        var row = "";
+                        $.each(result, function (index, item) {
+                            var cmnt = item.Comments == null ? "" : item.Comments;
+                            row += "<tr><td>" 
+                                + item.Id + "</td><td>" 
+                                + item.Register_Category + "</td><td>" 
+                                + item.Register_Type + "</td><td>" 
+                                + item.Register_Balance + "</td><td>" 
+                                + item.Register_Bill + "</td><td>" 
+                                + Date.parse(item.Register_Date) + "</td><td>" 
+                                + cmnt + "</td><td>"
+                                + "<p><a href=\"/Home/RegisterDelete/@item.Id\">Localized text</a></p>"
+                                + "</td></tr>";
+                        });
+                        $("#registerlist").html(row);
+                    //===============================================================================
+                   // $("#myPartialViewContainer").html("/Home/Index  ");
+                    
+                    //location.reload();
+
+                 },
+                async: true,
+                processData: false
+            });             
+}
 
